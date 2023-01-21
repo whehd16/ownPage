2b1ec0e1d541bf659f3b277fde6f347eb82ef93a
donghee <donghee@donghee.local>
2023-01-21 22:57:16 +0900

    [update] 20230121 skills

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const info = () => {
   return (
-    <div>
+    <div className='about__info grid'>
+        <div className="about__box">
+            <i class='bx bx-award about__icon'></i>
+            <h3 className="about__title">Experience</h3>
+            <span className="about__subtitle">8 Years Working</span>
+        </div>
+        
+        <div className="about__box">
+            <i class='bx bx-briefcase-alt-2 about__icon' ></i>
+            <h3 className="about__title">Complete</h3>
+            <span className="about__subtitle">48 + Projects</span>
+        </div>
+
+        <div className="about__box">
+            <i class='bx bx-support about__icon' ></i>
+            <h3 className="about__title">Support</h3>
+            <span className="about__subtitle">Online 24/7</span>
+        </div>
       
     </div>
   )
