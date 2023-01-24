ce72391b22fad63a76b0327513a38ed83e6a31e7
donghee <donghee@donghee.local>
2023-01-24 23:39:56 +0900

    [update] 20230124

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -53,6 +53,15 @@ const Backend = () => {
                         <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check' ></i>
+
+                    <div>
+                        <h3 className="skills__name">SQL</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
